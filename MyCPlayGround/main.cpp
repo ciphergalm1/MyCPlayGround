cec351890d211ce668fc8970787afc1aaf9efa2e
ciphergalm1 <adfxciphergalm1@outlook.com>
2016-07-14 12:48:06 +0900

    update 2 dimension array pointer access

---
+++ MyCPlayGround/main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<random>
+
+using namespace std;
+
+void printBiArray(int* head, int rows, int columns) {
+	//using pointer to traverse
+	for (int* row = head; row!= head+ rows; row++) {
+		for (int* p = row; p != head+ rows + columns;p++) {
+			cout << *p ;
+			cout << ' ';
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	// input: a binary data array with its address , rows and columns
+	// output: print all values
+
+	int rows, columns;
+	cout << "Please enter the rows and columns for your array:" << endl;
+	cin >> rows >> columns;
+	int** numList = new int*[rows];
+	for (int i = 0; i < rows; i++) {
+		numList[i] = new int[columns];
+	}
+
+	random_device rd;
+	mt19937 myGen;
+	myGen.seed(rd());
+	uniform_int_distribution<> diff(1,50);
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			numList[i][j] = diff(myGen);
+			cout << numList[i][j];
+			cout << ' ';
+		}
+		cout << endl;
+	}
+
+	int* head = &numList[0][0];
+	printBiArray(head,rows,columns);
+
+	for (int i = 0; i < rows; i++) {
+		delete[] numList[i];
+	}
+	delete[] numList;
+	return 0;
+}
